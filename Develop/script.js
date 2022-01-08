6c40253f3cd7e64175b266da6d9199c2dd568bfd
Max <maxfrank13@gmail.com>
2022-01-08 16:43:21 -0500

    finished styles and cleaned up getPassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,103 +1,112 @@
-// **** Form selectors ****
-const form = document.querySelector(".form")
-const createBtn = document.querySelector("#create");
-const generateBtn = document.querySelector("#generate");
-let passwordTextBox = document.querySelector(".password")
+window.addEventListener("DOMContentLoaded", function () {
 
-// **** Form inputs ****
-let desiredLength = document.getElementById("character-count");
-let includeUpperCase = document.getElementById("upper-case");
-let includeLowerCase = document.getElementById("lower-case");
-let includeNumbers = document.getElementById("numbers");
-let includeSpecChars = document.getElementById("spec-chars");
-let selectAll = document.getElementById("select-all");
+  // **** Form selectors ****
+  const form = document.querySelector(".form")
+  const formHeader = document.querySelector(".form-header");
+  const createBtn = document.querySelector("#create");
+  const generateBtn = document.querySelector("#generate");
+  const passwordTextBox = document.querySelector(".password")
+  const numberDisplay = document.querySelector("#number-display");
 
-// **** Button Listeners ****
-generateBtn.addEventListener("click", function () {
-  reset();
-  generateBtn.classList.toggle("hide");
-  createBtn.classList.toggle("hide");
-  form.classList.remove("hide");
-  passwordTextBox.classList.remove("hide");
-})
+  // **** Form inputs ****
+  const desiredLength = document.getElementById("character-count");
+  const includeUpperCase = document.getElementById("upper-case");
+  const includeLowerCase = document.getElementById("lower-case");
+  const includeNumbers = document.getElementById("numbers");
+  const includeSpecChars = document.getElementById("spec-chars");
+  const selectAll = document.getElementById("select-all");
 
-createBtn.addEventListener("click", function () {
-  if (checkVals()) {
-    getPassword(desiredLength.value, includeUpperCase.checked, includeLowerCase.checked, includeNumbers.checked, includeSpecChars.checked);
+  // **** Button Listeners ****
+  generateBtn.addEventListener("click", function () {
+    reset();
     generateBtn.classList.toggle("hide");
     createBtn.classList.toggle("hide");
-    form.classList.add("hide");
-  } else {
-    alert("please select at least 1 of the provided criteria")
-  }
-});
+    form.classList.remove("hide");
+    passwordTextBox.classList.add("hide");
+    formHeader.style.opacity = "0.7";
+  })
 
-selectAll.addEventListener("click", function(e) {
-  if (e.target.checked === true) {
-    includeUpperCase.checked = true;
-    includeLowerCase.checked = true;
-    includeNumbers.checked = true;
-    includeSpecChars.checked = true;
-  } else {
+  createBtn.addEventListener("click", function () {
+    if (checkVals()) {
+      getPassword(desiredLength.value, includeUpperCase.checked, includeLowerCase.checked, includeNumbers.checked, includeSpecChars.checked);
+      generateBtn.classList.toggle("hide");
+      createBtn.classList.toggle("hide");
+      form.classList.add("hide");
+      passwordTextBox.classList.remove("hide");
+    } else {
+      alert("please select at least 1 of the provided criteria")
+    }
+  });
+
+  // default display
+  numberDisplay.textContent = desiredLength.value;
+
+  desiredLength.addEventListener("click", function () {
+    numberDisplay.textContent = desiredLength.value;
+  })
+
+  selectAll.addEventListener("click", function (e) {
+    if (e.target.checked === true) {
+      includeUpperCase.checked = true;
+      includeLowerCase.checked = true;
+      includeNumbers.checked = true;
+      includeSpecChars.checked = true;
+    } else {
+      includeUpperCase.checked = false;
+      includeLowerCase.checked = false;
+      includeNumbers.checked = false;
+      includeSpecChars.checked = false;
+    };
+  })
+
+  // **** Functions ****
+
+  function getPassword(length, upper, lower, nums, specs) {
+    let newPasswordArray = []
+    let newPassword = "";
+
+    for (let i = 0; newPasswordArray.length < length; i++) {
+      let newVal = String.fromCharCode(Math.floor((Math.random() * 95) + 32));
+      if (
+        upper === false && /[A-Z]/.test(newVal) ||
+        lower === false && /[a-z]/.test(newVal) ||
+        nums === false && /[\d]/.test(newVal) ||
+        specs === false && /[/s!"#$%&'()*+,-./:;<=>?@\\[\]^_`{|}~]/.test(newVal)
+        ) { }
+      else if (specs === true && /\s/.test(newVal)) {
+        newPasswordArray.push("\xa0");
+      }
+      else {
+        newPasswordArray.push(newVal);
+      }
+    }
+    newPassword = newPasswordArray.join("");
+    passwordTextBox.textContent = newPassword;
+
+    console.log(newPassword);
+  }
+
+  function reset() {
     includeUpperCase.checked = false;
     includeLowerCase.checked = false;
     includeNumbers.checked = false;
     includeSpecChars.checked = false;
-  };
-})
+    selectAll.checked = false;
+    passwordTextBox.textContent = "";
+  }
 
-// **** Functions ****
-
-function getPassword(length, upper, lower, nums, specs) {
-  let newPasswordArray = []
-  let newPassword = "";
-
-  for (let i = 0; newPasswordArray.length < length; i++) {
-    let newVal = String.fromCharCode(Math.floor((Math.random() * 95) + 32));
-    if (upper === false && /[A-Z]/.test(newVal)) { }
-    else if (lower === false && /[a-z]/.test(newVal)) { }
-    else if (nums === false && /[\d]/.test(newVal)) { }
-    else if (specs === false && /[ !"#$%&'()*+,-./:;<=>?@\\[\]^_`{|}~]/.test(newVal)) { }
-    else {
-      newPasswordArray.push(newVal);
+  function checkVals() {
+    if (
+      includeUpperCase.checked === false &&
+      includeLowerCase.checked === false &&
+      includeNumbers.checked === false &&
+      includeSpecChars.checked === false
+    ) {
+      return false;
+    } else {
+      return true;
     }
   }
-  newPassword = newPasswordArray.join("");
-  passwordTextBox.textContent = newPassword;
 
-  console.log(newPassword);
-  console.log(typeof newPassword);
-}
-
-function reset() {
-  includeUpperCase.checked = false;
-  includeLowerCase.checked = false;
-  includeNumbers.checked = false;
-  includeSpecChars.checked = false;
-  selectAll.checked = false;
-  passwordTextBox.textContent = "";
-}
-
-function checkVals() {
-  if (
-    includeUpperCase.checked === false &&
-    includeLowerCase.checked === false &&
-    includeNumbers.checked === false &&
-    includeSpecChars.checked === false
-  ) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function massSelect() {
-  if (selectAll.checked === true) {
-    includeUpperCase.checked = false;
-    includeLowerCase.checked = false;
-    includeNumbers.checked = false;
-    includeSpecChars.checked = false;
-  }
-}
-
-
+})
+// end of window listener
